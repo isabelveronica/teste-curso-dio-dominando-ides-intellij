93a42900223dd4cec51ea93ebdbaab7cec811085
isabelveronica <isabelvfr@gmail.com>
2022-07-21 17:18:01 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroProjeto.java
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -8,8 +8,50 @@ public class PrimeiroProjeto {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numpag;
+
+    public Livro(String nome, Integer numpag) {
+        this.nome = nome;
+        this.numpag = numpag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(Integer numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag=" + numpag +
+                '}';
+    }
+}
+
+
+
+
+
